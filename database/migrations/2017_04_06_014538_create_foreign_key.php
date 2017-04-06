e339ce882ec4ce636cd7e14f8fd4b74218078ad8
felliperey <a10007@bcc.unifal-mg.edu.br>
2017-04-05 22:48:34 -0300

    Fixing hazards

--- database/migrations/2017_04_06_014538_create_foreign_key.php
+++ database/migrations/2017_04_06_014538_create_foreign_key.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateForeignKeys extends Migration
+class CreateForeignKey extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateForeignKeys extends Migration
      */
     public function up()
     {
-        /*Schema::table('accidents', function ($table) {
+        Schema::table('accidents', function ($table) {
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
         });
 
@@ -61,7 +61,49 @@ class CreateForeignKeys extends Migration
         Schema::table('rules', function ($table) {
             $table->foreign('variable_id')->references('id')->on('variables')->onDelete('cascade');
             $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade');
-        });*/
+        });
+
+        Schema::table('actuators', function ($table) {
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+
+        Schema::table('controllers', function ($table) {
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+
+        Schema::table('controlled_processes', function ($table) {
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+
+        Schema::table('sensors', function ($table) {
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+
+        Schema::table('connections', function ($table) {
+            $table->foreign('output_component_id')->references('id')->on('controllers')->onDelete('cascade');
+            $table->foreign('output_component_id')->references('id')->on('actuators')->onDelete('cascade');
+            $table->foreign('output_component_id')->references('id')->on('controlled_processes')->onDelete('cascade');
+            $table->foreign('output_component_id')->references('id')->on('sensors')->onDelete('cascade');
+            $table->foreign('input_component_id')->references('id')->on('controllers')->onDelete('cascade');
+            $table->foreign('input_component_id')->references('id')->on('actuators')->onDelete('cascade');
+            $table->foreign('input_component_id')->references('id')->on('controlled_processes')->onDelete('cascade');
+            $table->foreign('input_component_id')->references('id')->on('sensors')->onDelete('cascade');
+        });
+
+        Schema::table('context_tables', function (Blueprint $table) {
+            $table->foreign('controlaction_id')->references('id')->on('control_actions')->onDelete('cascade');
+        });
+
+        Schema::table('safety_constraints', function (Blueprint $table) {
+            $table->foreign('controlaction_id')->references('id')->on('control_actions')->onDelete('cascade');
+            $table->foreign('rule_id')->references('id')->on('rules')->onDelete('cascade');
+        });
+
+        Schema::table('causal_analysis', function (Blueprint $table) {
+            $table->foreign('guideword_id')->references('id')->on('guidewords')->onDelete('cascade');
+            $table->foreign('safety_constraint_id')->references('id')->on('safety_constraints')->onDelete('cascade');
+        });
+        
     }
 
     /**
