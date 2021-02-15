270dc8b2f06333b9cc7c8df8d2a14d55eba7253e
Fellipe Rey <a10007@bcc.unifal-mg.edu.br>
2021-02-15 15:17:11 -0300

    Fixing STPA Step 3 timeout bug

--- resources/views/pages/stepthree.blade.php
+++ resources/views/pages/stepthree.blade.php
@@ -6,38 +6,49 @@
 		@include('partials.stepthree.select-control-action')
 	</div>
 
-	@foreach(App\Controllers::where('project_id', $project_id)->get() as $controller)
-		@foreach(App\ControlAction::where('controller_id', $controller->id)->get() as $ca)
-			<div id="control-action-{{$ca->id}}" class="hide-control-actions" name="control-action-{{$ca->id}}" hidden="true" style="display:none;" >
+	<?php
+		$isValidParameters = isset($_GET["ca"]) && is_numeric($_GET["ca"]) && isset($_GET["controller"]) && is_numeric($_GET["controller"]);
 
-				<div class="substep substep--addrule" id="addrule">
-					@include('partials.stepthree.addrules')
-				</div>
+		if ($isValidParameters) {
+			$ca_id = $_GET["ca"];
+			$controller_id = $_GET["controller"];
 
-				<div class="substep substep--rule" id="rule-control-action-{{$ca->id}}">
-					@include('partials.stepthree.rules')
-				</div>
+			$controller = App\Controllers::where('id', $controller_id)->first();
+			$ca = App\ControlAction::where('id', $ca_id)->first();
+		} else {
+			$controller = App\Controllers::where('project_id', $project_id)->first();
+			$ca = App\ControlAction::where('controller_id', $controller->id)->first();
+		}
+	?>
 
-				<div class="substep substep--context-table" id="context-table">
-					@include('partials.stepthree.context-table')
-				</div>
+	<div id="control-action-{{$ca->id}}" class="hide-control-actions" name="control-action-{{$ca->id}}" hidden="true" style="display:none;" >
 
-				<div class="substep substep--add-uca" id="add-uca-control-action-{{$ca->id}}">
-					@include('partials.stepthree.add-uca', ['controller' => $controller, 'controlaction' => $ca])
-				</div>
+	<div class="substep substep--addrule" id="addrule">
+			@include('partials.stepthree.addrules')
+	</div>
 
-				<div class="substep substep--safety-constraints" id="safety-constraints">
-					@include('partials.stepthree.safety-constraints')
-				</div>
+	<div class="substep substep--rule" id="rule-control-action-{{$ca->id}}">
+			@include('partials.stepthree.rules')
+	</div>
 
-				<!-- commented blade code -->
-				{{-- @include('partials.stepthree.suggested-uca', ['controller' => $controller, 'controlaction' => $ca]) ---}} 
-				@include('partials.stepthree.edit-uca', ['controller' => $controller, 'controlaction' => $ca])
+	<div class="substep substep--context-table" id="context-table">
+			@include('partials.stepthree.context-table')
+	</div>
+
+	<div class="substep substep--add-uca" id="add-uca-control-action-{{$ca->id}}">
+			@include('partials.stepthree.add-uca', ['controller' => $controller, 'controlaction' => $ca])
+	</div>
+
+	<div class="substep substep--safety-constraints" id="safety-constraints">
+			@include('partials.stepthree.safety-constraints')
+	</div>
+
+	<!-- commented blade code -->
+	{{-- @include('partials.stepthree.suggested-uca', ['controller' => $controller, 'controlaction' => $ca]) ---}} 
+	@include('partials.stepthree.edit-uca', ['controller' => $controller, 'controlaction' => $ca])
 
 
-			</div>
-		@endforeach
-	@endforeach
+	</div>
 
 @endsection
 
