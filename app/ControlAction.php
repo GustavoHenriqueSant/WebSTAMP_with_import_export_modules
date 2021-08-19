abede944534395ff9f20de0ca7b55ee75ecb2e68
GustavoHenriqueSant <gustavo.git@hotmail.com>
2021-08-19 18:13:07 -0300

    [relacoes model ContrlAction] Adição de todas as relações que Control Action possui

--- app/ControlAction.php
+++ app/ControlAction.php
@@ -10,4 +10,15 @@ class ControlAction extends Model
 		return $this->belongsTo(Controllers::class);
 	}
 
+	public function rules(){
+		return $this->hasMany(Rule::class, "controlaction_id");
+	}
+
+	public function contextTable(){
+		return $this->hasMany(ContextTable::class, "controlaction_id");
+	}
+
+	public function safetyConstraint(){
+		return $this->hasMany(SafetyConstraints::class, "controlaction_id");
+	}
 }
