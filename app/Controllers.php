e00c96fdb143cef7b5d5aa9341950c3e3058bbfc
GustavoHenriqueSant <gustavo.git@hotmail.com>
2021-08-19 22:02:35 -0300

    [relaçoes model controller] Adição de todas as rel~ções que controller possui

--- app/Controllers.php
+++ app/Controllers.php
@@ -10,4 +10,12 @@ class Controllers extends Model
 		return $this->belongsTo(Projects::class);
 	}
 
+	public function controlaction(){
+		return $this->hasMany(ControlAction::class, "controller_id");
+	}
+
+	public function variables(){
+		return $this->hasMany(Variable::class, "controller_id");
+	}
+
 }
