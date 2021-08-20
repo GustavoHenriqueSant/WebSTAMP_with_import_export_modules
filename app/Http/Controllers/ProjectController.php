37de58097fd75e3eb186c5ff5d7fd2f370fc0fdd
GustavoHenriqueSant <gustavo.git@hotmail.com>
2021-08-19 23:24:28 -0300

    [Project controller] Adição da função referente a funcionalidade de exportar em JSON

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -41,6 +41,19 @@ class ProjectController extends Controller
 		return redirect(route('projects'));
 	}
 
+	public function export(Request $request){
+		$project = Project::where('id',  $request->get('project_id'))->
+							with("actuators", "assumptions", "connection", "controlledProcess", "controllers",
+								"controllers.controlaction", "controllers.controlaction.rules", "controllers.controlaction.rules.variableState",
+								"controllers.controlaction.contextTable",
+								"controllers.controlaction.safetyConstraint", "controllers.controlaction.safetyConstraint.causalAnalysis",
+								"controllers.controlaction.safetyConstraint.rulesSafetyConstraintsHazards", "controllers.variables",
+								"controllers.variables.state", "hazards", "hazards.losseshazards",
+								"losses", "missions", "sensor", "systemGoals", "ssc", "ssc.hazards")->first();
+
+		return json_encode($project);
+	}
+
 	public function delete(Request $request){
 		Project::destroy($request->get('project_id'));
 
